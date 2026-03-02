3853d19709d0a772fabeecaba405d70b977c3459
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2026-03-02 08:53:50 +0530

    Added public keys for deployment

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export var CONFIG = {
+    NEWS_API_KEY: "3f9b9c5e-129b-43ff-97a8-00fce8dd4a47",
+    NEWS_BASE_URL: "https://eventregistry.org/api/v1/article/getArticles",
+    BASE_NEWS_COUNT: 21,
+    BASE_KEYWORD: 'news'
+};
